3ea61f74d0ca0999afe2cc21fe3f33d51b1087d3
Lu Weeke <luweike64@gmail.com>
2024-05-16 14:34:12 +0800

    add webSocket

---
+++ cmd/websocket/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"nhooyr.io/websocket"
+	"nhooyr.io/websocket/wsjson"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "HTTP, Hello")
+	})
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := websocket.Accept(w, r, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer conn.Close(websocket.StatusInternalError, "internal err")
+
+		ctx, cancel := context.WithTimeout(r.Context(), time.Second*10)
+		defer cancel()
+
+		var v interface{}
+		err = wsjson.Read(ctx, conn, &v)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Printf("Recived client: %v\n", v)
+
+		err = wsjson.Write(ctx, conn, "Hello WebSocket Client")
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		conn.Close(websocket.StatusNormalClosure, "")
+	})
+
+	log.Fatal(http.ListenAndServe(":2021", nil))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/wekeeroad/GoSocket
 
 go 1.22.1
+
+require nhooyr.io/websocket v1.8.11
